f5bc36ce517836ca2f6c4a88ce4c2294aa3d0a44
suhasmukund <suhasmukund208@gmail.com>
2023-02-17 10:03:56 +0530

    added trail.cpp

---
+++ trail.cpp
@@ -0,0 +1,9 @@
+//File: trail.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Iam PES1UG20CS440\n";
+  cout << "Build using Jenkins Pipelines\n";
+  return 0;}
